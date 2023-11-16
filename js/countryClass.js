3acdbb356ab0c44b9edd47a7b028bf7d7725155e
אודיה סעדון <osadoun@g.jct.ac.il>
2023-11-16 20:13:38 +0200

    fix syntax in countryClass

--- js/countryClass.js
+++ js/countryClass.js
@@ -1,30 +1,30 @@
-export default class Country{
-    constructor(_parent, _country){
-        this.name = _country.name.common;
-        this.population = _country.population;
-        this.flag = _country.flags.png;
-        this.flag_alt = _country.flags.alt;
-        this.capital = _country.capital;
-        this.lat = _country.latlng ? _country.latlng[0] : undefined ;
-        this.lon = _country.latlng ? _country.latlng[1] : undefined ;
-        this.lang = _country.languages ? Object.keys(_country.languages) : undefined ;
-        this.borders = _country.borders;
-        this.parent = _parent;
-    }
+export default class Country {
+  constructor(_parent, _country) {
+    this.name = _country.name.common;
+    this.population = _country.population;
+    this.flag = _country.flags.png;
+    this.flag_alt = _country.flags.alt;
+    this.capital = _country.capital;
+    this.lat = _country.latlng ? _country.latlng[0] : undefined;
+    this.lon = _country.latlng ? _country.latlng[1] : undefined;
+    this.lang = _country.languages
+      ? Object.keys(_country.languages)
+      : undefined;
+    this.borders = _country.borders;
+    this.parent = _parent;
+  }
 
-    render(){
-        let div = document.createElement("div");
-        console.log(this.lat, this.lon);
-        div.innerHTML = `
-        <div
-        class="map col-md-6 d-flex align-items-center my-5 px-2"
-      >
-
-            <iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
-            src="https://maps.google.com/maps?q=${this.lat},${this.lon}&hl=en&z=6&amp;output=embed">
-            </iframe>
-           </div>
-
+  render() {
+    let div = document.createElement("div");
+    console.log(this.lat, this.lon);
+    div.innerHTML = `
+      <div class="map col-md-6 d-flex align-items-center my-5 px-2">
+        <iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
+            src="https://maps.google.com/maps?q=${this.lat},${
+      this.lon
+    }&hl=en&z=6&amp;output=embed">
+        </iframe>
+      </div>
       <div class="country_single col-md-6 align-items-center my-5 px-2">
         <div class="flag-out">
           <div class="flag border-bottom border-top border-end">
@@ -32,24 +32,24 @@ export default class Country{
           </div>
           <div class="country_info">
             <h2 class="display-6 fw-bold text-center p-4">${this.name}</h2>
-            <p class="lead px-5">Population: ${Number(this.population).toLocaleString()}</p>
+            <p class="lead px-5">Population: ${Number(
+              this.population
+            ).toLocaleString()}</p>
             <p class="lead px-5">Capital: ${this.capital}</p>
             <p class="lead px-5">Language: ${this.lang}</p>
             <p class="lead px-5">Borders: ${this.borders}</p>
           </div>
         </div>
       </div>
-        `;
-        div.className = "container-info d-flex mx-auto justify-content-between";
+      `;
+    div.className = "container-info d-flex mx-auto justify-content-between";
+    document.querySelector(this.parent).append(div);
+  }
 
-        document.querySelector(this.parent).append(div);
-
-    }
-
-    renderSmallInfo(showCountry){
-        let country = document.createElement("div");
-        country.className = "country card col-lg-2 col-md-4 mx-3 my-5  p-3 ";
-        country.innerHTML = `
+  renderSmallInfo(showCountry) {
+    let country = document.createElement("div");
+    country.className = "country card col-lg-2 col-md-4 mx-3 my-5  p-3 ";
+    country.innerHTML = `
         <div class="d-flex align-items-center image_flag_small_info">
         <img src="${this.flag}" class="card-img-top" alt="${this.flag_alt}" />
         </div>
@@ -58,12 +58,11 @@ export default class Country{
           <a href="#" class="btn btn-outline-secondary">For more info</a>
         </div>
         `;
+    country.querySelector("a").addEventListener("click", () => {
+      showCountry(this.name);
+    });
 
-        country.querySelector("a").addEventListener("click", () =>{
-            showCountry(this.name);
-        })
-
-        console.log(this.parent);
-        document.querySelector(this.parent).append(country);
-    }
+    console.log(this.parent);
+    document.querySelector(this.parent).append(country);
+  }
 }
